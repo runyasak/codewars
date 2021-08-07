fa5ec9f8d013169b3e4e5bbc23abd06df1d675e8
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-08-07 14:44:11 +0700

    divisors: implement empty?

--- ruby/divisors.rb
+++ ruby/divisors.rb
@@ -1,5 +1,6 @@
 def divisors(n)
-  selectDivisors = [*2..n - 1].select {|num| n % num == 0}
-  result = (selectDivisors.length == 0) ? "#{n} is prime" : selectDivisors
-  return result
+  result = [*2..n - 1].select {|num| n % num == 0}
+  (result.empty?) ? "#{n} is prime" : result
 end
+
+puts divisors(10)
